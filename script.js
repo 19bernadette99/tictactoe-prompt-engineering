8c74bd457dbaf517740fbc06d57338d0801cfda9
Bernadette Pöltl <bernadette.poeltl99@gmail.com>
2024-12-07 15:45:12 +0100

    clean code

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ let fields = [
 ];
 
 const WINNING_COMBINATIONS = [
-    [0, 1, 2], [3, 4, 5], [6, 7, 8], // horizontal
-    [0, 3, 6], [1, 4, 7], [2, 5, 8], // vertical
-    [0, 4, 8], [2, 4, 6], // diagonal
+    [0, 1, 2], [3, 4, 5], [6, 7, 8], 
+    [0, 3, 6], [1, 4, 7], [2, 5, 8], 
+    [0, 4, 8], [2, 4, 6], 
 ];
 
 let currentPlayer = 'circle';
@@ -73,7 +73,7 @@ function handleClick(cell, index) {
     updatePlayerIndicator();
 
     if (isSinglePlayer && currentPlayer === 'cross' && gameActive) {
-        setTimeout(robotMove, 500); // Roboterzug mit Verzögerung
+        setTimeout(robotMove, 500); 
     }
 }
 
